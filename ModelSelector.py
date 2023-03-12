27e7a6989591e69a33e8944e50733f32d4d38f6b
shyamsivasankar <shyamshivshankar33@gmail.com>
2023-03-12 17:46:21 +0530

    config changes for hosting

--- ModelSelector.py
+++ ModelSelector.py
@@ -8,7 +8,7 @@ from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.exponential_smoothing.ets import ETSModel 
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from sklearn.metrics import mean_absolute_percentage_error
-from xgboost import XGBRegressor
+# from xgboost import XGBRegressor
 
 def arima(train,test):
     model = ARIMA(train, order=(3,0,1))
@@ -69,16 +69,16 @@ def SARIMA(train,test):
             'Predictions' : predicted_data,
             'ModelObj' : model_fit}
 
-def XGB(train,test):
-    model = XGBRegressor(n_estimators=1000)
-    # print('---------------------------------------------------------------------------------------------',train.columns)
-    model_fit = model.fit(train.index , train['point_value'])
-    predicted_data = model_fit.predict(test.index)
-    error = mean_absolute_percentage_error(test, predicted_data)
-    return {'model' : 'XGB', 
-            'MAPE' : error, 
-            'Predictions' : predicted_data,
-            'ModelObj' : model_fit}
+# def XGB(train,test):
+#     model = XGBRegressor(n_estimators=1000)
+#     # print('---------------------------------------------------------------------------------------------',train.columns)
+#     model_fit = model.fit(train.index , train['point_value'])
+#     predicted_data = model_fit.predict(test.index)
+#     error = mean_absolute_percentage_error(test, predicted_data)
+#     return {'model' : 'XGB', 
+#             'MAPE' : error, 
+#             'Predictions' : predicted_data,
+#             'ModelObj' : model_fit}
 
 def SelectModel(df):
     trainlen = int(len(df)*0.8)
